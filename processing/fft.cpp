8950904190b7a01e3e8c4ea71622e0252361debf
svdprima <svdvl@hotmail.com>
2017-04-13 11:12:35 +0300

    Added a new folder

---
+++ processing/fft.cpp
@@ -0,0 +1,67 @@
+#include <complex>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <pthread.h>
+#include "dft.h"
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include "sampling.h"
+
+sum_info::sum_info (): raw (0, 0, 0), image (NULL), odds (NULL), evens (NULL), sample_nu (0), core_nu (0), my_nu (0)
+{
+}
+
+sum_info::sum_info (amplitude_probes r, std::vector <std::complex <double> > *i, std::vector <std::complex <double> > *o,
+		    std::vector <std::complex <double> > *e, unsigned int s, unsigned int c, unsigned int m): raw (r), image (i), odds (o), evens (e), sample_nu (s), core_nu (c), my_nu (m)
+{
+}
+//using Cooley-Tukey algorithm
+void* calculate (void* info)
+{
+	sum_info* dat = (sum_info*) info;
+	for (unsigned int i = dat->my_nu; i < dat->sample_nu; i += dat->core_nu)
+	{
+		for (unsigned int j = 0; j < dat->sample_nu/2; j++)
+		{
+			(*(dat->evens))[i] += std::complex <double> (dat->raw[2 * j]) * exp (std::complex <double> (0, -4 * M_PI * j * i /dat->sample_nu));
+			(*(dat->odds))[i] += std::complex <double> (dat->raw[2 * j + 1]) * exp (std::complex <double> (0, -4 * M_PI * j * i / dat->sample_nu));
+		}
+	}
+	for (unsigned int i = dat->my_nu; i < dat->sample_nu / 2; i += dat->core_nu)
+	{
+		(*(dat->image)) [i] = (*(dat->evens))[i] + exp(std::complex <double>(0, -2 * M_PI * i / dat->sample_nu)) * (*(dat->odds)) [i];
+		(*(dat->image)) [i + dat->sample_nu / 2] = (*(dat->evens))[i] - exp(std::complex <double>(0, -2 * M_PI * i / dat->sample_nu)) * (*(dat->odds)) [i];
+
+	}
+	return NULL;
+}	
+
+void dft(amplitude_probes& proto, std::vector <std::complex <double> > &image, const unsigned int core_nu)
+{
+	const unsigned int sample_nu = proto.count;
+	std::vector <std::complex <double> > odds (sample_nu);
+	std::vector <std::complex <double> > evens (sample_nu);
+	std::vector <pthread_t> thr (core_nu);
+	std::vector <sum_info> inf (core_nu);
+	for (unsigned int i = 0; i < core_nu; i++)
+	{
+		inf[i] = sum_info (proto, &image, &odds, &evens, sample_nu, core_nu, i);
+		errno = pthread_create (thr.data() + i, NULL, calculate, (void*) (&inf[i]));
+		if (errno)
+		{
+			std::cout << strerror (errno);
+			return;
+		}
+	}
+	for (unsigned int i = 0; i < core_nu; i++)
+	{
+		errno = pthread_join (thr [i], NULL);
+		if (errno)
+		{
+			std::cout << strerror (errno);
+			return;
+		}
+	}
+}
---
+++ processing/main.cpp
@@ -0,0 +1,54 @@
+#include <complex>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <errno.h>
+#include <unistd.h>
+#include "dft.h"
+#include "sampling.h"
+
+#define SINGLE_CORE
+
+#ifdef SINGLE_CORE
+void fft (amplitude_probes& proto, std::vector <std::complex <double> > &image)
+{
+	for (unsigned int i = 0; i < proto.count; i++)
+		for (unsigned int j = 0; j < proto.count - 1; j++)
+		{
+			image [i] += std::complex <double>(proto [j]) * exp (std::complex <double> (0, -2 * M_PI * j * i / proto.count));  
+		}
+}
+#endif
+
+int main ()
+{
+	#ifndef SINGLE_CORE
+	const unsigned int cores = sysconf (_SC_NPROCESSORS_ONLN);
+	#endif
+	const unsigned long int sz = 4096; 
+	const unsigned long int discr_freq = 44100;
+	std::vector <double> freq (sz/2);
+	for (unsigned int i = 0; i < sz / 2; i++)
+		freq[i] = (double) discr_freq / sz * i;
+	amplitude_probes amp = amplitude_probes (44100, sz, 2);
+	syncmaster* sync = amp.capture (1);
+	while (pthread_mutex_trylock (&sync->is_done) == EBUSY)
+	{
+	}
+	std::vector <std::complex <double> > out (sz);
+	#ifdef SINGLE_CORE
+	fft (amp, out);
+	#else 
+	dft (amp, out, cores);
+	#endif
+	{
+		std::ofstream display;
+		display.open ("output.txt");
+		for (unsigned int i = 0; i < sz/2; i++)
+			display << freq[i] <<" "<<std::abs (out[i]) <<"\n";
+		display.close ();
+	}
+	return 0;
+}
+
