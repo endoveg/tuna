3a296da7ed1572f7be654b0ea933eec3e724fff7
Egor Endovitskiy <leftwin@yandex.ru>
2017-04-24 23:15:42 +0300

    - filter rewritten - pics added Now it uses IIR filter, because FIR required too large order (about 600) and we don't need linear phase response. IIR filter implementaion is cascade application of second order filters. Every second order filter is computed in Transposed Direct Form II.

--- processing/filter_test.cpp
+++ processing/filter_test.cpp
@@ -7,16 +7,30 @@ double last_found_freq = 440.0;
 
 int main() {
   std::ifstream filter_coefs;
+  std::ofstream non_filtered;
+  std::ofstream filtered;
+  filtered.open("filtered.out");
+  non_filtered.open("non_filtered.out");
   filter_coefs.open("coef");
   filter F(filter_coefs);
-  for(int i = 0; i < 100; i++) {
-    amplitude_probes *amp = new amplitude_probes(22050, 4096, 2);
+  while(1) {
+    amplitude_probes *amp = new amplitude_probes(44100, 4096, 2);
     amp->capture(1);
     pthread_mutex_lock(&(amp->is_done));
+    int M = 11;
+    for (int i = M; i < amp->count; i++) {
+      non_filtered << (*amp)[i] << std::endl;
+    }
     F.apply(amp);
-    yin(amp, 0.2,1102,0); //it creats d_t_tau (t=0) //551 * 2 = 1102
+    for (int i = M ; i < amp->count; i++) {
+      filtered << amp->filtered[i] << std::endl;
+      }
+    return(0);
+    yin(*amp,0.2,1102,0); //it creats d_t_tau (t=0) //551 * 2 = 1102
+    return(0);
+    //return(0);
     for (int i = 1; i < 1102; i++) {
-      printf("%.2f\n", yin(amp, 0.2, 1102, i));
+      printf("%.2f\n", yin(*amp, 0.2, 1102, i));
     }
   }
 }
